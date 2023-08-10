75695a4ac29623b15a4d711a748a384cb04526f0
medilies <medilies@gmail.com>
2023-08-10 22:12:49 +0100

    comments

--- src/Context.php
+++ src/Context.php
@@ -31,16 +31,21 @@ class Context
 
     private function createWindow(): static
     {
+        // OpenGL spec
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, $this->max_version);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, $this->min_version);
         glfwWindowHint(GLFW_OPENGL_PROFILE, $this->profile);
 
+        // Resizable
         glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+        // Required to run on Mac OS X
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
         $this->window = new Window(800, 600, 'Hello PHP GLFW');
+        // glViewport(0,0,800,600); // ! This breaks the process
 
+        // This function makes the OpenGL or OpenGL ES context of the specified window current on the calling thread. A context must only be made current on a single thread at a time and each thread can have only a single current context at a time.
         $this->window->makeCurrentContext();
 
         return $this;
