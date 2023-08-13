dc81230ee0cd79ea2d5b57f994eb37c1354531d5
medilies <medilies@gmail.com>
2023-08-13 19:35:19 +0100

    refactor ShaderProgram

--- src/ShaderProgram.php
+++ src/ShaderProgram.php
@@ -6,68 +6,75 @@ use Exception;
 
 class ShaderProgram
 {
-    private mixed $program;
+    private readonly int $program;
 
-    public function __construct(
-        private readonly string $vertexName,
-        private readonly string $fragmentName
-    ) {
-        $vertexShader = $this->compileVertex();
-        $fragShader = $this->compileFragment();
+    public function __construct(string $vertexName, string $fragmentName)
+    {
+        $this->program = glCreateProgram();
 
-        $this->createProgram($vertexShader, $fragShader);
+        // create, upload and compile the shaders
+        $vertexShader = $this->compileVertex($vertexName);
+        $fragShader = $this->compileFragment($fragmentName);
+
+        $this->linkProgram($vertexShader, $fragShader);
 
         // free the shaders
         glDeleteShader($vertexShader);
         glDeleteShader($fragShader);
     }
 
-    private function compileVertex(): mixed
+    private function compileVertex(string $vertexName): int
     {
-        // create, upload and compile the vertex shader
-        $vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        $shaderCode = file_get_contents(__DIR__."/assets/shaders/vertex/{$vertexName}.glsl");
 
-        glShaderSource($vertexShader, file_get_contents(__DIR__."/assets/shaders/vertex/{$this->vertexName}.glsl"));
+        return $this->compileShader(GL_VERTEX_SHADER, $shaderCode);
+    }
+
+    private function compileFragment(string $fragmentName): int
+    {
+        $shaderCode = file_get_contents(__DIR__."/assets/shaders/fragment/{$fragmentName}.glsl");
+
+        return $this->compileShader(GL_FRAGMENT_SHADER, $shaderCode);
+    }
+
+    private function compileShader(int $type, string $shaderCode): int
+    {
+        $vertexShader = glCreateShader($type);
+
+        glShaderSource($vertexShader, $shaderCode);
 
         glCompileShader($vertexShader);
         glGetShaderiv($vertexShader, GL_COMPILE_STATUS, $success);
 
         if (! $success) {
-            throw new Exception('Vertex shader could not be compiled.');
+            throw new Exception(($type === GL_VERTEX_SHADER ? 'Vertex' : 'Fragment').' shader could not be compiled.');
         }
 
         return $vertexShader;
     }
 
-    private function compileFragment(): mixed
+    /**
+     * create a shader program and link our vertex and fragment shader together
+     */
+    private function linkProgram(int $vertexShader, int $fragShader): void
     {
-        // create, upload and compile the fragment shader
-        $fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-        glShaderSource($fragShader, file_get_contents(__DIR__."/assets/shaders/fragment/{$this->fragmentName}.glsl"));
-
-        glCompileShader($fragShader);
-        glGetShaderiv($fragShader, GL_COMPILE_STATUS, $success);
-
-        if (! $success) {
-            throw new Exception('Fragment shader could not be compiled.');
-        }
-
-        return $fragShader;
-    }
-
-    private function createProgram(mixed $vertexShader, mixed $fragShader): void
-    {
-        // create a shader program and link our vertex and fragment shader together
-        $this->program = glCreateProgram();
         glAttachShader($this->program, $vertexShader);
         glAttachShader($this->program, $fragShader);
+
         glLinkProgram($this->program);
-
         glGetProgramiv($this->program, GL_LINK_STATUS, $linkSuccess);
-
         if (! $linkSuccess) {
             throw new Exception('Shader program could not be linked.');
+        }
+
+        // ! not confident of the following code to access logs
+        glValidateProgram($this->program);
+        glGetProgramiv($this->program, GL_VALIDATE_STATUS, $validationSuccess);
+        if (GL_TRUE !== $validationSuccess) {
+            glGetShaderiv($this->program, GL_INFO_LOG_LENGTH, $length);
+
+            echo glGetShaderInfoLog($this->program, $length).PHP_EOL;
+            throw new Exception('Shader program is not valid.');
         }
     }
 
@@ -84,7 +91,7 @@ class ShaderProgram
         return $this->program;
     }
 
-    public function delete()
+    public function delete(): void
     {
         glDeleteProgram($this->program);
     }
